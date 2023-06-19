2841f9347c3294acda41cd0739f7db131960721a
Oliver Lim <metaa0213@gmail.com>
2023-06-19 18:00:39 +0900

    init Sample Project

---
+++ src/main/java/com/metlife/azuredevopsspringboot/AzureDevopsSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.metlife.azuredevopsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AzureDevopsSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureDevopsSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metlife/azuredevopsspringboot/AzureDevopsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metlife.azuredevopsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDevopsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
